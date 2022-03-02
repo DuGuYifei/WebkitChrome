3d24dc0d498822636050f7b3d2bb7bac89e53ecc
DuGuYifei <liuyifei97@163.com>
2022-03-02 22:22:34 +0100

    create

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+This a small webkit chrome browser which is use [CefSharp](http://cefsharp.github.io/). It can be downloaded by using **NuGet** manager (for C# in VS) which is something like Noedejs(for javascript).
+
+It doesn't add url input box and navigation arrow. 
+
+If want to add it, only need add ths C# .Net controls and give them sefsharp api's function.
---
+++ WebkitChrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebkitChrome
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length == 0)
+            {
+                //args = new string[] { "dicegame.xyz" };
+                args = new string[] { "E:\\Download\\Wallpaper\\html\\example\\ripples\\index.html" };
+                //args = new string[] { "E:\\图片\\大圣归来\\1.jpg" };
+                //args = new string[] { "E:\\Gdansk University of Technology\\first semester\\Hypertext and Hypermadia\\Project\\Project1\\HTML\\MyHobby.html" };
+            }
+            Application.Run(new WebkitChromeForm(args[0]));
+        }
+    }
+}
